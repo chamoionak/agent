bacfe7baca8244ca792d3550df3d4b4e8582f068
chamoionak <2591558509@qq.com>
2026-05-15 00:04:39 +0800

    AI应用开发1

---
+++ src/test/java/com/yupi/yuaiagent/app/LoveAppTest.java
@@ -0,0 +1,33 @@
+package com.yupi.yuaiagent.app;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LoveAppTest {
+
+    @Resource
+    private LoveApp loveApp;
+    @Test
+    void doChat() {
+        String chatId = UUID.randomUUID().toString();
+
+        //第一轮
+        String message="你好，我是程序员小蝈蝈";
+        String answer= loveApp.doChat(message,chatId);
+        //第二轮
+        message="你找找个看我的另一半是谁";
+        answer= loveApp.doChat(message,chatId);
+        Assertions.assertNotNull(answer);
+        //第三轮
+        message="你好，你记得我叫什么吗";
+        answer= loveApp.doChat(message,chatId);
+        Assertions.assertNotNull(answer);
+    }
+}
